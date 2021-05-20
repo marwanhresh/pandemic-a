70ea035d59613b99d89ca5647b4078c3cc721baf
marwanhreish <62513189+marwanhresh@users.noreply.github.com>
2021-05-20 12:49:56 +0300

    Update Dispatcher.cpp

--- Dispatcher.cpp
+++ Dispatcher.cpp
@@ -1,21 +1,31 @@
 #include "Dispatcher.hpp"
-using namespace pandemic;
-Dispatcher::Dispatcher(Board& b,int city):Player(b,city)
-{
-    //ctor
-}
+#include<limits>
 
-Dispatcher::~Dispatcher()
-{
-    //dtor
-}
-Player& Dispatcher::fly_direct(int c){
-    if(board.is_lab(current_city)){
-        current_city=c;
-        return *this;
-    }else
-        return Player::fly_direct(c);
-}
-string Dispatcher::role(){
-    return "Dispatcher";
+using namespace std;
+namespace pandemic{
+
+    Dispatcher::Dispatcher(Board &board, City city): Player(board, city) {
+        this->_role="Dispatcher";
+    }
+
+    Player &Dispatcher::fly_direct(City city) {
+        if(city==this->curr_city){
+            throw invalid_argument("can't fly same city");
+        }
+
+        if(this->board.is_research_station(this->curr_city)){
+            this->curr_city=city;
+            return *this;
+        }
+
+        if(this->player_cards.find(city)!=this->player_cards.end()){
+            this->curr_city = city;
+            this->player_cards.erase(city);
+            this->number_of_cards_by_color[Board::city_color(city)]--;
+            return *this;
+        }
+        throw invalid_argument("not research station");
+
+    }
+
 }
