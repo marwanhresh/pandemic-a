5317e833ae0cbdb301e946eaa4de22540d9bb4f5
marwanhreish <62513189+marwanhresh@users.noreply.github.com>
2021-05-20 12:51:12 +0300

    Update FieldDoctor.cpp

--- FieldDoctor.cpp
+++ FieldDoctor.cpp
@@ -1,27 +1,31 @@
 #include "FieldDoctor.hpp"
-#include "MyException.h"
-using namespace pandemic;
-FieldDoctor::FieldDoctor(Board& b,int c):Player(b,c)
-{
+#include<limits>
 
-}
+using namespace std;
+namespace pandemic {
 
-FieldDoctor::~FieldDoctor()
-{
 
-}
-Player& FieldDoctor::treat(int c){
-    if(c==current_city){
-        return Player::treat(c);
+    FieldDoctor::FieldDoctor(Board &board, City city) : Player(board, city) {
+        this->_role = "FieldDoctor";
     }
-    if(board.get_health(c)==0||board.can_drive(current_city,c)==0)
-        throw MyException("there is no infection in the city or trying to treat not connected city");
-    if( board.is_discovered_cure(board.get_color(c)) )
-        board[c]=0;
-    else
-        board[c]=board[c]-1;
-    return *this;
-}
-string FieldDoctor::role(){
-    return "FieldDoctor";
+
+    Player &FieldDoctor::treat(City city) {
+        if (city == curr_city) {
+            Player::treat(city);
+            return *this;
+        }
+        if (!Board::is_connected(city, curr_city)) {
+            throw invalid_argument("cities are not neighbors");
+        }
+        if (board[city] == 0) {
+            throw invalid_argument("Disease is already 0");
+        }
+        if (this->board.is_there_cure(Board::city_color(city))) {
+            board[city] = 0;
+        } else {
+            board[city]--;
+        }
+        return *this;
+    }
+
 }
