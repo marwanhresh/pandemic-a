e3d859fe42f37875762a30cbbe34350eba866093
marwanhreish <62513189+marwanhresh@users.noreply.github.com>
2021-05-20 12:46:39 +0300

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -1,31 +1,34 @@
-#include<iostream>
-#ifndef BOARD_HPP
-#define BOARD_HPP
-
-using namespace std;
-namespace pandemic{
-class Board
-{
+#ifndef UNTITLED5_BOARD_HPP
+#define UNTITLED5_BOARD_HPP
+#include <ostream>
+#include "City.hpp"
+#include "Color.hpp"
+#include <set>
+#include <map>
+namespace pandemic {
+    class Board {
+    private:
+        std::map<City, bool> research_stations;
+        std::map<City, int> disease_cubes;
+        std::set<Color> disease_cures;
+        bool clean= true;//flag if board is clean
+        int id;
     public:
         Board();
-        virtual ~Board();
-        int& operator[](int);
-        int can_drive(int,int);
-        int get_health(int);
-        int get_color(int);
-        void build(int);
-        int is_lab(int);
-        int is_clean();
-        void discover_cure(int);
-        int is_discovered_cure(int);
-        friend ostream& operator<<(ostream&,Board&);
+        void build(City city);
+        bool is_clean();
         void remove_cures();
-    private:
-        int* cities_health;
-        int* cities_labs;
-        int* colors;
-        int* cures;
-        int** drives;
-};
+        bool is_there_cure(Color color);
+        void set_cure(Color color);
+        int &operator[](City city);
+        void remove_stations();
+        friend std::ostream &operator<<(std::ostream &out, Board &board);
+        bool is_research_station(City city);
+        void add_research_station(City city);
+        static Color city_color(City city);
+        static bool is_connected(City city_a,City city_b);
+
+    };
+
 }
-#endif
+#endif //UNTITLED5_BOARD_HPP
