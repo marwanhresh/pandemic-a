664cca345c2ac7fc05202da3b4ef4a2b49d90937
marwanhreish <62513189+marwanhresh@users.noreply.github.com>
2021-05-20 12:56:39 +0300

    Update OperationsExpert.hpp

--- OperationsExpert.hpp
+++ OperationsExpert.hpp
@@ -1,11 +1,17 @@
+#ifndef OPERATIONSEXPERT_HPP
+#define OPERATIONSEXPERT_HPP
+
 #include "Player.hpp"
-namespace pandemic{
-class OperationsExpert:public Player
-{
+#include "Board.hpp"
+
+namespace pandemic {
+    class OperationsExpert : public Player {
     public:
-        OperationsExpert(Board&,int);
-        virtual ~OperationsExpert();
-        void build();
-        string role();
-};
+        OperationsExpert(Board &board, City city) ;
+        Player& build() override;
+
+    };
 }
+
+
+#endif //UNTITLED5_OPERATIONSEXPERT_HPP
