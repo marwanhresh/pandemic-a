b55306c8074e0c3bfd75ae7c2406b2fdc05375ee
marwanhreish <62513189+marwanhresh@users.noreply.github.com>
2021-05-20 12:57:13 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,85 +1,132 @@
 #include "Player.hpp"
-#include <iostream>
-#include "MyException.h"
-using namespace pandemic;
-Player::Player(Board& b,int city):board(b),current_city(city)
-{
-    cards=new int[48];
-    count_colors=new int[4];
-    for(int i=0;i<48;i++)
-        cards[i]=0;
-    for(int i=0;i<4;i++)
-        count_colors[i]=0;
+#include<limits>
 
+
+using namespace std;
+
+
+pandemic::Player::Player(pandemic::Board& b, City city) : board(b), curr_city(city), _role("None") {
+    this->number_of_cards_by_color[Yellow]=0;
+    this->number_of_cards_by_color[Black]=0;
+    this->number_of_cards_by_color[Blue]=0;
+    this->number_of_cards_by_color[Red]=0;
 }
 
-Player::~Player()
-{
-    delete[] cards;
-    delete[] count_colors;
-}
-void Player::build(){
-    if(board.is_lab(current_city))
-        throw MyException("the city already have a lab\n");
-    if(cards[current_city]==0)
-        throw MyException("don't have a card to build\n");
-    board.build(current_city);
-}
-Player& Player::fly_direct(int c){
-    if(cards[c]==1){
-        drop_card(c);
-        current_city=c;
-    }else
-        throw MyException("dont have a card of direction city to fly direct\n");
-    return *this;
-}
-void Player::discover_cure(Color color){
-    if(board.is_lab(current_city)&&count_colors[static_cast<int>(color)]>=5){
-        board.discover_cure(static_cast<int>(color) );
-        for(int i=0,c=0;i<48&&c<5;i++)
-            if( board.get_color(cards[i])==static_cast<int>(color) ){
-                drop_card(cards[i]);
-                c++;
-            }
-    }else
-        throw MyException("don't have enough cards to discover cure OR do't have lab in the current city\n");
-}
-Player& Player::treat(int c){
-    if(board.get_health(current_city)==0||c!=current_city)
-        throw MyException("there is no infection on the city or trying to treat not current city");
-    else if( board.is_discovered_cure(board.get_color(current_city)) )
-        board[current_city]=0;
-    else
-        board[current_city]=board[current_city]-1;
-    return *this;
-}
-Player& Player::drive(int c){
-    if(board.can_drive(current_city,c)){
-        current_city=c;
-    }else
-        throw MyException("cannot drive to not connected city");
-        return *this;
-}
-Player& Player::take_card(int card){
-    cards[card]=1;
-    count_colors[board.get_color(card)]+=1;
-    return *this;
-}
-void Player::drop_card(int card){
-    cards[card]=0;
-    count_colors[board.get_color(card)]-=1;
-}
-void Player::fly_charter(int c){
-    if(cards[current_city]==0)
-        throw MyException("don't have a card of current city to fly charter");
-    else{
-        drop_card(current_city);
-        current_city=c;
+pandemic::Player &pandemic::Player::fly_direct(pandemic::City city) {
+    if(city==this->curr_city){
+        throw invalid_argument("can't fly same city");
     }
+    if(this->player_cards.find(city)!=this->player_cards.end()) {//if the player has this card
+        this->curr_city = city;
+        this->player_cards.erase(city);
+        this->number_of_cards_by_color[Board::city_color(city)]--;
+    }else{
+        throw invalid_argument("U don't have a valid card for this flight!");
+    }
+    return *this;
 }
-void Player::fly_shuttle(int c){
-    if(board.is_lab(c)&&board.is_lab(current_city)){
-        current_city=c;
-    }else
-        throw MyException("don't have lab in the current city or in direct city cannot fly shuttle");
+
+pandemic::Player &pandemic::Player::fly_charter(pandemic::City city) {
+    if(city==this->curr_city){
+        throw invalid_argument("can't fly same city");
+    }
+    if(this->player_cards.find(curr_city)!=this->player_cards.end()) {
+        this->player_cards.erase(this->curr_city);
+        this->number_of_cards_by_color[Board::city_color(curr_city)]--;
+
+        this->curr_city = city;
+    }else{
+        throw invalid_argument("U don't have a valid card for this flight!");
+    }
+    return *this;
+}
+
+pandemic::Player &pandemic::Player::fly_shuttle(pandemic::City city) {
+    if(city==this->curr_city){
+        throw invalid_argument("can't fly same city");
+    }
+    if(!this->board.is_research_station(this->curr_city)||!this->board.is_research_station(city)) {
+        throw invalid_argument("for this flight U need to have research stations in both cities");
+    }
+    this->curr_city=city;
+    return *this;
+
+}
+
+pandemic::Player &pandemic::Player::discover_cure(pandemic::Color color) {
+    if(this->board.is_there_cure(color))
+    {
+        return *this;
+    }
+    if(this->number_of_cards_by_color[color]<num_five) {//there is not enough cards
+        throw invalid_argument("there is not enough cards");
+    }
+    if(!this->board.is_research_station(this->curr_city)) {//if there is no research station
+        throw invalid_argument("there is no Research station in this city");
+    }
+    unordered_set<City> temp;
+    for(const auto &v: this->player_cards){
+        if(pandemic::Board::city_color(v)==color){
+            this->number_of_cards_by_color[color]--;
+            temp.insert(v);
+        }
+    }
+    for(const auto &v: temp) {
+        this->player_cards.erase(v);
+    }
+
+    this->board.set_cure(color);
+    return *this;
+}
+
+pandemic::Player &pandemic::Player::treat(City city) {
+    if(city!=curr_city){
+        throw invalid_argument("U are not in this city");
+    }
+    if(board[city]==0){
+        throw invalid_argument("Disease is already 0");
+    }
+    if(this->board.is_there_cure(Board::city_color(curr_city))){
+        board[city]=0;
+    } else{
+        this->board[city]--;
+    }
+    return *this;
+}
+
+pandemic::Player &pandemic::Player::take_card(pandemic::City city) {
+    if(this->player_cards.find(city)==this->player_cards.end()) {//if the the card does not exists
+        this->number_of_cards_by_color[pandemic::Board::city_color(city)]++;
+        this->player_cards.insert(city);
+    }
+    return *this;
+}
+
+pandemic::Player &pandemic::Player::drive(pandemic::City city) {
+    if(!Board::is_connected(this->curr_city,city)) {
+        throw invalid_argument("cities are not neighbors");
+    }
+    this->curr_city=city;
+    return *this;
+}
+
+pandemic::Player &pandemic::Player::build() {
+    if(this->board.is_research_station(this->curr_city)){
+        return *this;
+        }
+    if(this->player_cards.find(curr_city)==this->player_cards.end()) {
+        throw invalid_argument("U don't have a valid card for this Operation");
+    }
+    this->board.add_research_station(this->curr_city);
+    this->player_cards.erase(curr_city);
+    this->number_of_cards_by_color[Board::city_color(curr_city)]--;
+    return *this;
+}
+
+std::string &pandemic::Player::role() {
+    return this->_role;
+}
+
+void pandemic::Player::remove_cards() {
+    this->player_cards.clear();
 }
